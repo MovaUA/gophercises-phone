e129d550a1e16686602c7c0ca735eb9d880c208c
mova <valeriy.molchanov.77@gmail.com>
2021-02-06 21:58:23 +0200

    undo

--- pkg/phone/phone.go
+++ pkg/phone/phone.go
@@ -1,15 +1,19 @@
 // Package phone provides functions to manipulate phone numbers
 package phone
 
-import "unicode"
+import (
+	"bytes"
+	"unicode"
+)
 
 // Norm normalizes the provided phone number
 func Norm(phoneNo string) string {
-	result := make([]rune, 0, len(phoneNo))
+	b := bytes.NewBuffer(make([]byte, 0, len(phoneNo)))
 	for _, r := range phoneNo {
 		if !unicode.IsDigit(r) {
 			continue
 		}
-		result = append(result, r)
-	return string(result)
+		b.WriteRune(r)
+	}
+	return b.String()
 }
