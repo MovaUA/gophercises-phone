a85fae392d4a5b6714be3635211f58f66a0829bd
mova <valeriy.molchanov.77@gmail.com>
2021-02-06 21:53:24 +0200

    review

--- pkg/phone/phone_test.go
+++ pkg/phone/phone_test.go
@@ -7,24 +7,25 @@ import (
 )
 
 func TestNorm(t *testing.T) {
-	tests := []struct {
-		actual string
-		want   string
+	testCases := []struct {
+		input string
+		want  string
 	}{
-		{actual: "1234567890", want: "1234567890"},
-		{actual: "123 456 7891", want: "1234567891"},
-		{actual: "(123) 456 7892", want: "1234567892"},
-		{actual: "(123) 456-7893", want: "1234567893"},
-		{actual: "123-456-7894", want: "1234567894"},
-		{actual: "123-456-7890", want: "1234567890"},
-		{actual: "1234567892", want: "1234567892"},
-		{actual: "(123)456-7892", want: "1234567892"},
+		{input: "1234567890", want: "1234567890"},
+		{input: "123 456 7891", want: "1234567891"},
+		{input: "(123) 456 7892", want: "1234567892"},
+		{input: "(123) 456-7893", want: "1234567893"},
+		{input: "123-456-7894", want: "1234567894"},
+		{input: "123-456-7890", want: "1234567890"},
+		{input: "(123)456-7892", want: "1234567892"},
+		{input: "there is no phone here", want: ""},
+		{input: "", want: ""},
 	}
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.actual, func(t *testing.T) {
-			if got := phone.Norm(tt.actual); got != tt.want {
-				t.Fatalf("want %q, got %q", tt.want, got)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			if got := phone.Norm(tc.input); got != tc.want {
+				t.Fatalf("want %q, got %q", tc.want, got)
 			}
 		})
 	}
